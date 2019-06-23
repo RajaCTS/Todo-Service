25796fedec403b667e19a5a26b50826a37092a40
RajaCTS <rajadurai2093@gmail.com>
2019-06-23 23:16:09 +0530

    Atlas connect

--- commonModule/mongooseConnect.js
+++ commonModule/mongooseConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect('mongodb+srv://admin:admin@todo-qhnad.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true});
 
 module.exports = { mongoose };
